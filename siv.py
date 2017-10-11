675c8f22677c09a36d47405174b612dca5ff83fc
balaji yalla <chinnabalaji.yalla@gmail.com>
2017-10-11 13:15:26 +0200

    Update siv.py

--- siv.py
+++ siv.py
@@ -10,3 +10,20 @@ import hashlib
 import time
 
 start_time = time.time()
+try:                               #creates history.txt file if not exists
+  f=open("history.txt","r")
+  f.close()
+except:
+  f=open("history.txt","w")
+  f.close()
+
+f=open("history.txt","r")         #finding no.of lines in the file to write monitored directories in an order
+lines=f.readlines()
+lastid=len(lines)+1
+f.close()
+
+def directory_exists(directory):  #checks if directory exists or not?
+    if(os.path.isdir(directory)):
+      return True
+    else:
+      return False
